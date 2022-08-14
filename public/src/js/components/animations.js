c0e1de0983d740d17f666a5e9128bc71b50016ca
Stephen Anindo <sanindo@hotmail.co.uk>
2022-08-14 10:36:48 +0100

    first commit

---
+++ public/src/js/components/animations.js
@@ -0,0 +1,231 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
+const html = document.querySelector("html");
+html.setAttribute("lang", "en");
+
+// GSAP and handle resize bug
+$(document).ready(function() {
+			gsap.registerPlugin(ScrollTrigger);
+      
+      ScrollTrigger.defaults({
+      		markers: false
+      });
+});
+
+// Hero background animation 
+
+$(".sticky-section").each(function (index) {
+  let triggerElement = $(this);
+  let targetElement = $(".sticky-section_image");
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      // trigger element - viewport
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 1
+    }
+  });
+  tl.to(targetElement, {
+    opacity:"1",
+    backgroundSize: "110vw",
+    duration: 1,
+  });
+});
+
+// About scroll animation 
+gridAnim();
+function gridAnim() {
+$(".section.section--about").each(function (index) {
+  let triggerElement = $(this);
+  let background = $(".grid_item");
+	let images = $(".grid_image");
+  let text = $(".scroll-anim")
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      // trigger element - viewport
+      start: "top top",
+      end: "bottom bottom+=150px",
+      scrub: 1
+    }
+  });
+  tl.from(background, {
+    x: -50,
+    opacity: 0,
+    stagger: 0.15,
+    duration: 1,
+    force3D: false,
+  });
+  tl.from(images, {
+    x: -30,
+    opacity: 0,
+    stagger: 0.15,
+    duration: 1,
+    force3D: false,
+  });
+});
+}
+
+// Basic Animations
+/* 
+** Slide animations
+*/
+gsap.set(".slide-in", {y: 25, opacity: 0});
+      ScrollTrigger.batch(".slide-in", {
+      start: "top bottom-=100px",
+      onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, duration: 1, stagger: 0.15}),
+      onLeaveBack: batch => gsap.to(batch, {opacity: 0, y: 25, duration: 1, stagger: 0.1})
+});
+
+gsap.set(".slide-in--late", {opacity: 0, scale:0.9});
+      ScrollTrigger.batch(".slide-in--late", {
+      start: "top 50%",
+      onEnter: batch => gsap.to(batch, {opacity: 1, scale:1, duration: 0.7, stagger: 0.15}),
+      onLeaveBack: batch => gsap.to(batch, {opacity: 0, scale:0.9, duration: 0.7, stagger: 0.1})
+});
+
+gsap.set(".slide-down", {y: -25, opacity: 0});
+      ScrollTrigger.batch(".slide-down", {
+      start: "top bottom-=100px",
+      onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, duration: 1, stagger: 0.15}),
+      onLeaveBack: batch => gsap.to(batch, {opacity: 0, y: -25, duration: 1, stagger: 0.1})
+});
+
+gsap.set(".platforms", {width: "0%", opacity: 0});
+      ScrollTrigger.batch(".platforms", {
+      start: "top bottom-=100px",
+      onEnter: batch => gsap.to(batch, {opacity: 1, width: "80%", duration: 0.5, stagger: 0.15}),
+      onLeaveBack: batch => gsap.to(batch, {opacity: 0, width: "0%", duration: 1, stagger: 0.1})
+});
+
+// Block animations
+/* 
+** Music Block Animation
+*/
+
+let musicBlock = document.querySelectorAll(".music-block")
+
+musicBlock.forEach( (element) => {
+	let title = element.querySelectorAll(".music-block_title")
+  let text = element.querySelectorAll(".music-block_text")
+  let videos = element.querySelectorAll(".music-block_video")
+	let images = element.querySelectorAll(".music-block_image")
+  let platforms = element.querySelectorAll(".platform-list_image")
+  
+  // timeline 
+	let tl = gsap.timeline()
+	  .from(videos, {
+    opacity: 0,
+    y: 25,
+    duration: 1,
+    })
+    .from(images, {
+    opacity: 0,
+    y: 25,
+    duration: 1,
+    }, 0)
+    .from(title, {
+    y: 10, 
+    opacity: 0,
+    delay: 0.2,
+    duration: 0.85,
+    }, "-=0.85")
+    .from(text, {
+    y: 20,
+    opacity: 0,
+    duration: 0.95,
+    }, "-=0.95")
+    .from(platforms, {
+    y: 10, 
+    opacity: 0,
+    duration: 0.75,
+    },"-=0.55")
+	
+	ScrollTrigger.create({
+		trigger:element,
+		start:"top 50%",
+		toggleActions:"play none none reverse",
+		animation:tl
+	})
+})
+
+/* 
+** TV Block Animation
+*/
+let tvBlock = document.querySelectorAll(".tv-list_item")
+
+tvBlock.forEach( (element) => {
+	let title = element.querySelectorAll(".tv-block_title")
+  let subtitle = element.querySelectorAll(".tv-block_subtitle")
+	let images = element.querySelectorAll(".tv-block_link")
+  
+  // timeline 
+	let tl = gsap.timeline()
+	  .from(images, {
+    opacity: 0,
+    y: 25,
+    duration: 1,
+    stagger: 0.1,
+    })
+		.from(title, {
+    y: 15, 
+    opacity: 0,
+    duration: 1,
+    stagger: 0.1,
+    }, "-=0.75")
+    .from(subtitle, {
+    y: 15, 
+    opacity: 0,
+    duration: 0.75,
+    stagger: 0.1,
+    }, "-=0.85")
+	
+	ScrollTrigger.create({
+		trigger:element,
+		start:"top 50%",
+		toggleActions:"play none none reverse",
+		animation:tl
+	})
+})
+
+// Event image animation 
+$(".content-block.is--half").each(function (index) {
+  let triggerElement = $(this);
+  let targetElement = $(".event_image");
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      // trigger element - viewport
+      start: "top 30%",
+      end: "bottom bottom",
+      scrub: 1
+    }
+  });
+  tl.from(targetElement, {
+    opacity:"0",
+    y: 25,
+    duration: 1,
+  });
+});
+
+// Footer Parallax animation 
+const tl = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".footer",
+		start: "top top",
+		end: "bottom top",
+		scrub: true
+	}
+});
+
+gsap.utils.toArray(".parallax").forEach(layer => {
+	const depth = layer.dataset.depth;
+	const movement = -(layer.offsetHeight * depth)
+	tl.to(layer, {y: movement, ease: "none"}, 0)
+});
